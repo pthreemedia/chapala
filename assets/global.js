b43e83ccb8643cd98618d91a725c41de4b64bc2f
Vojkan <vojkan@pthreemedia.com>
2022-05-24 16:19:30 -0400

    prevent select size

--- assets/global.js
+++ assets/global.js
@@ -746,7 +746,23 @@ customElements.define('slideshow-component', SlideshowComponent);
 class VariantSelects extends HTMLElement {
   constructor() {
     super();
+
+    this.initOptions();
+
+    if (!this.currentVariant) {
+      this.toggleAddButton(true, 'Select a size', true);
+    } else {
+      this.updateVariantInput();
+      this.renderProductInfo();
+    }
+
     this.addEventListener('change', this.onVariantChange);
+  }
+
+  initOptions() {
+    // This function is left empty in VariantSelects object
+    // It is to be overridden in VariantRadios object
+    return;
   }
 
   onVariantChange() {
@@ -895,10 +911,24 @@ class VariantRadios extends VariantSelects {
     super();
   }
 
+  initOptions() {
+    const sizeFieldset = this.querySelector("fieldset.product-form-Size");
+
+    if(sizeFieldset) {
+      const fieldInputs = sizeFieldset.querySelectorAll("input");
+      if(fieldInputs.length > 1) {
+        const activeFieldInput = Array.from(fieldInputs).find((input) => input.hasAttribute("checked"));
+        if(activeFieldInput) activeFieldInput.checked = false;
+      }
+    }
+  }
+
   updateOptions() {
     const fieldsets = Array.from(this.querySelectorAll('fieldset'));
     this.options = fieldsets.map((fieldset) => {
-      return Array.from(fieldset.querySelectorAll('input')).find((radio) => radio.checked).value;
+      // return Array.from(fieldset.querySelectorAll('input')).find((radio) => radio.checked).value;
+      const checkedInput = Array.from(fieldset.querySelectorAll("input")).find((radio) => radio.checked);
+      return checkedInput ? checkedInput.value : "";
     });
   }
 }
